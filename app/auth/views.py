febbfd4dd6e26928b045a8a90bd2fb67e8125915
Junkyu Park <junk3843@naver.com>
2017-04-25 00:44:45 +0900

    Implement user registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,9 +5,13 @@ from flask import render_template
 from . import auth
 
 from flask import redirect, request, url_for, flash
-from flask_login import login_user, login_required, logout_user
+from flask_login import login_user, login_required, logout_user, \
+        current_user
+from .. import db
 from ..models import User
 from .forms import LoginForm
+from ..email import send_email
+from .forms import LoginForm, RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -39,3 +43,17 @@ def logout():
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
 
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can now login.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', form=form)
