29758c539798ce4e77e8ad9e5d93401649c3b681
Junkyu Park <junk3843@naver.com>
2017-04-25 02:38:25 +0900

    Implement operation reseting password

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -38,3 +38,25 @@ class ChangePasswordForm(Form):
         Required(), EqualTo('password2', message='Passwords must match')])
     password2 = PasswordField('Confirm new password', validators=[Required()])
     submit = SubmitField('Update Password')
+
+class PasswordResetRequestForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
+
+
+class PasswordResetForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    password = PasswordField('New Password', validators=[
+        Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Reset Password')
+
+    """
+    접두어 validate_로 되어있는 메소드를 정의할 때, 이 메소드는 검증자를 정의하도록
+    추가적으로 호출된다.
+    """
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first() is None:
+            raise ValidationError('Unknown email address.')
